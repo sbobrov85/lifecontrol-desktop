8be710e9f06f2a3ae3070d64fd09f561e852df9f
sbobrov85 <sbobrov85@gmail.com>
2017-01-26 06:41:44 +0500

    #27 Display simple main tabs interface

---
+++ src/main/java/com/jerait/lifecontrol/desktop/controller/MainTabsController.java
@@ -0,0 +1,19 @@
+package com.jerait.lifecontrol.desktop.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ */
+public class MainTabsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
